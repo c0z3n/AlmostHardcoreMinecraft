e63413cbe4d0de44a14c0f05213765a0b8f84334
Gordo <gordon.p.clement@gmail.com>
2015-10-26 20:47:59 -0400

    added keeping track of players deaths and total deaths
    also commands to display that information back to players

--- AlmostHardcoreMinecraft/src/org/c0z3n/AlmostHardcore.java
+++ AlmostHardcoreMinecraft/src/org/c0z3n/AlmostHardcore.java
@@ -7,13 +7,17 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,8 +26,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class AlmostHardcore extends JavaPlugin{
-	
+public class AlmostHardcore extends JavaPlugin{	
 	
 	HashMap<UUID, Integer[]> spawnData = new HashMap<UUID, Integer[]>();
 	@Override
@@ -37,6 +40,11 @@ public class AlmostHardcore extends JavaPlugin{
 				// triggered when any player dies
 				Player player = e.getEntity();
 				updateGlobalSpawnLocation(player);
+				Integer spnum = getConfig().getInt("spawnCount."+player.getName());
+				Integer stnum = getConfig().getInt("spawnCount.death-total");
+				getConfig().set("spawnCount."+player.getName(), spnum + 1);
+				getConfig().set("spawnCount.death-total", stnum + 1);
+			    saveConfig();
 			}
 			@EventHandler
 			public void onJoin(PlayerJoinEvent e) {
@@ -45,17 +53,34 @@ public class AlmostHardcore extends JavaPlugin{
 				if((spawnData.get(player.getUniqueId()) == null) || (!player.hasPlayedBefore())){
 					updatePlayerSpawnData(player, getServer().getWorlds().get(0).getSpawnLocation());
 				}
+				getConfig().addDefault("spawnCount."+player.getName(), 0);
+			    saveConfig();
+				
 			}
 			@EventHandler
 			public void onSpawn(PlayerRespawnEvent e) {
 				Player player = e.getPlayer();
+				boolean newSpawn = true;
+				Integer spawnLocation[] = {e.getRespawnLocation().getBlockX(), e.getRespawnLocation().getBlockX()};
+				for (Integer v[] : spawnData.values()) {
+				    if(spawnProximityChecker(spawnLocation, v)){
+				    	newSpawn = false;
+				    }
+				}
+				if(newSpawn){
+					getServer().broadcastMessage(ChatColor.RED + e.getPlayer().getName() + " died and is respawning at a brand new spawn point!");
+					getServer().broadcastMessage(ChatColor.RED + "There may be some lag as the server generates new map tiles");
+				} 
 				updatePlayerSpawnData(player, e.getRespawnLocation());
 			}
+			
 			
 		}, this);
 		
 	    this.getConfig().addDefault("RandomSpawnWindowSize", 500000);
 	    this.getConfig().addDefault("TrackingDataFile", "trackingdata.dat");
+//	    this.getConfig().addDefault("spawnCount", 0);
+	    this.getConfig().addDefault("spawnCount.death-total", 0);
 	    this.getConfig().options().copyDefaults(true);
 	    this.getServer().setSpawnRadius(0);
 	    loadSpawnData();
@@ -66,6 +91,32 @@ public class AlmostHardcore extends JavaPlugin{
 	public void onDisable() {
 		saveSpawnData();
 		saveConfig();
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		Player player = (Player) sender;
+		if (cmd.getName().equalsIgnoreCase("deaths") && sender instanceof Player){
+			Integer total = getConfig().getInt("spawnCount.death-total");
+			Integer spawns = getConfig().getInt("spawnCount." + player.getName());
+			player.sendMessage(ChatColor.RED + "total deaths       : " + total.toString());
+			player.sendMessage(ChatColor.RED + "your deaths        : " + spawns.toString());
+		}
+
+		if (cmd.getName().equalsIgnoreCase("deathboard") && sender instanceof Player){
+			Map<String, Object> alldeaths = getConfig().getConfigurationSection("spawnCount").getValues(false);
+			getServer().broadcastMessage(ChatColor.RED + "---DEATH SCOREBOARD----");
+			for (Map.Entry<String, Object> entry : alldeaths.entrySet()) {
+				Integer spaces = 16 - entry.getKey().length();
+				String whitespace = "";
+				for(int i = 0; i< spaces; i++){
+					whitespace = whitespace + " ";
+				}
+				getServer().broadcastMessage(ChatColor.RED + entry.getKey() + whitespace + ": " + entry.getValue());
+			}
+		}
+		
+		
+		return false;
 	}
 
 	public int randCoord() {
